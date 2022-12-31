ef299e2b7cba0f6086daf6b244fd6bfaeab0d0e6
Timur Barmakov <tbarmakov@bk.ru>
2022-12-31 15:29:34 +0300

    Вывод списка компонентов с помощью  массива

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import Card from './components/Card';
 import Header from './components/Header';
 import Drawer from './components/Drawer';
 
+const arr = [
+  {title : 'Мужские Кроссовки Nike Blazer Mid Suede', price: 25600, imageUrl:'/img/sneakers/1.jpg'},
+  {title : 'Мужские Кроссовки Nike Air Max 270', price: 79999,imageUrl:'/img/sneakers/2.jpg'},
+  {title : 'Мужские Кроссовки Nike Blazer Mid Suede', price: 28599,imageUrl:'/img/sneakers/3.jpg'},
+  {title : 'Кроссовки Puma X Aka Boku Future Rider', price: 46999,imageUrl:'/img/sneakers/4.jpg'},
+
+]
+
 function App() {
   return (
     <div className="wrapper clear">
@@ -17,7 +25,15 @@ function App() {
       </div>
 
       <div className="d-flex">
-      <Card />
+    
+      {arr.map((obj) => (
+        <Card
+        title={obj.title}
+        price={obj.price}
+        imageUrl={obj.imageUrl}
+        onClick = {() => console.log(obj)}
+        />
+      ))}
       </div>
     </div>
     
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,17 +1,23 @@
-function Card() {
+import styles from './Card.module.scss'
+
+function Card(props) { 
+    // const onClickButton = () => {
+    //     alert(props.title);
+    // }
+
     return (
-        <div className="card">
+        <div className={styles.card}>
         <div className="favorite">
           <img src="/img/heart-unliked.svg" alt="Unliked" />
         </div>
-        <img width={133} height={112} src="/img/sneakers/1.jpg" alt="" />
-        <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+        <img width={133} height={112} src={props.imageUrl} alt="" />
+        <h5>{props.title}</h5>
         <div className="d-flex justify-between align-center">
           <div className="d-flex flex-column">
             <span>Цена:</span>
-            <b>59 999 тг.</b>
+            <b>{props.price} тг.</b>
           </div>
-          <button className="button">
+          <button className="button" onClick={props.onClick}>
             <img width={11} height={11} src="/img/plus.svg" alt="Plus" />
         </button>
           </div>
